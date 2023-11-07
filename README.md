806e7d65351bd5d315c869311f44562a085d7a68
Humberto Sales <betofoxmal@gmail.com>
2023-11-06 22:24:04 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Projeto landing page com Dev em Dobro!
+
+Os desafios do Dev em Dobro ajudam você a melhorar suas habilidades de codificação criando projetos realistas.
+
+## Layout do projeto em tela de Desktop/Notebook/Tablet/Mobile.
+
+<div align="center">
+
+  <img src="" width="400px"/>
+
+</div>
+
+### O que aprendi
+
+```Js
+  function mostarOuEsconderSetas(){
+    const naoEhAPrimeiraImagem = imagemAtual !== 0;
+    (naoEhAPrimeiraImagem) ? setaVoltar.classList.remove("opacidade") : setaVoltar.classList.add("opacidade");
+
+    const chegouNaUltimaImagem = imagemAtual !== 0 && imagemAtual === imagensPainel.length - 1;
+    (chegouNaUltimaImagem) ? setaAvancar.classList.add("opacidade") : setaAvancar.classList.remove("opacidade");
+  }
+```
+
+### Construído com
+
+- Marcação semântica HTML5
+- Propriedades personalizadas CSS
+- Caixa flexível
+- Js com Fetch e GitHub API
+
+## Desenvolvido em:
+
+<div>
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" width="30px"/>
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" width="30px"/>
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" width="30px"/>
+</div>
---
+++ src/js/painel.js
@@ -0,0 +1,56 @@
+const imagensPainel = document.querySelectorAll('.imagem-painel');
+const setaVoltar = document.getElementById('btn-voltar');
+const setaAvancar = document.getElementById('btn-avancar');
+
+let imagemAtual = 0;
+
+function esconderImagem() {
+    imagensPainel.forEach(imagem => {
+        imagem.classList.remove('mostrar')
+    })    
+}
+
+function mostrarImagem() {
+    imagensPainel[imagemAtual].classList.add('mostrar');
+}
+
+function mostarOuEsconderSetas(){
+    const naoEhAPrimeiraImagem = imagemAtual !== 0;
+    (naoEhAPrimeiraImagem) ? setaVoltar.classList.remove("opacidade") : setaVoltar.classList.add("opacidade");
+
+    const chegouNaUltimaImagem = imagemAtual !== 0 && imagemAtual === imagensPainel.length - 1;
+    (chegouNaUltimaImagem) ? setaAvancar.classList.add("opacidade") : setaAvancar.classList.remove("opacidade");
+}
+
+setaVoltar.addEventListener('click', function(){
+    
+    if (imagemAtual === 0) {
+        return;
+    }
+
+    imagemAtual--;
+
+    esconderImagem();
+
+    mostrarImagem();
+
+    mostarOuEsconderSetas();
+
+});
+
+setaAvancar.addEventListener('click', function(){
+    
+    const totalDeImagens = imagensPainel.length -1;
+    
+    if (imagemAtual === totalDeImagens) {
+        return;
+    }
+
+    imagemAtual++;
+
+    esconderImagem();
+
+    mostrarImagem();
+
+    mostarOuEsconderSetas();
+});
